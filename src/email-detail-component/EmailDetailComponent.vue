6e425729c3276f2fa35574de6c1472b8d58ec06c
Ed Pfromer <epfromer@gmail.com>
2018-10-16 18:31:39 -0600

    working on detail page

--- src/email-detail-component/EmailDetailComponent.vue
+++ src/email-detail-component/EmailDetailComponent.vue
@@ -1,7 +1,18 @@
 <template>
   <div v-if=this.$store.state.selectedEmail>
     {{ this.$store.state.selectedEmail.clientSubmitTime }}
-    <br />
+    <br/>
+    <strong>From: </strong>{{ this.$store.state.selectedEmail.senderEmailAddress }}
+    <br/>
+    <strong>To: </strong>{{ this.$store.state.selectedEmail.displayTo }}
+    <br/>
+    <strong>Subject: </strong>{{ this.$store.state.selectedEmail.subject }}
+    <br/>
+    <strong>CC: </strong>{{ this.$store.state.selectedEmail.displayCC }}
+    <br/>
+    <strong>BCC: </strong>{{ this.$store.state.selectedEmail.displayBCC }}
+    <br/>
+    <span v-html="formatBody(this.$store.state.selectedEmail.body)"></span>
 
   </div>
 </template>
@@ -9,13 +20,15 @@
 <script>
 export default {
   name: 'EmailDetailComponent',
-  props: {}
+  props: {},
+  methods: {
+    formatBody: body => body.replace(/\n/g, '<br />')
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 div {
-  border: 1px solid yellow;
 }
 </style>
