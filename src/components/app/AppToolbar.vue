2ded5aba313f86a8bec75885161f0388abbdf23e
Ed Pfromer <epfromer@gmail.com>
2020-12-03 14:18:21 -0700

    update

--- src/components/app/AppToolbar.vue
+++ src/components/app/AppToolbar.vue
@@ -3,24 +3,46 @@
     <v-app-bar-nav-icon @click.stop="() => setDrawerOpen(true)" />
     <v-toolbar-title>X2 Vue</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn @click="setDark" icon>
-      <v-icon>{{ darkMode ? 'brightness_7' : 'brightness_4' }}</v-icon>
-    </v-btn>
-    <v-btn
-      @click="() => $router.push(`/AppSettingsView`).catch((err) => {})"
-      icon
-    >
-      <v-icon>settings</v-icon>
-    </v-btn>
-    <v-btn @click="() => $router.push(`/`).catch((err) => {})" icon>
-      <v-icon>dashboard</v-icon>
-    </v-btn>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" @click="setDark" icon>
+          <v-icon>{{ darkMode ? 'brightness_7' : 'brightness_4' }}</v-icon>
+        </v-btn>
+      </template>
+      <span>Toggle light / dark theme</span>
+    </v-tooltip>
+    <settings-button />
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          @click="() => $router.push(`/SearchView`).catch((err) => {})"
+          icon
+        >
+          <v-icon>search</v-icon>
+        </v-btn>
+      </template>
+      <span>Search</span>
+    </v-tooltip>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          @click="() => $router.push(`/`).catch((err) => {})"
+          icon
+        >
+          <v-icon>home</v-icon>
+        </v-btn>
+      </template>
+      <span>x2 Home</span>
+    </v-tooltip>
   </v-app-bar>
 </template>
 
 <script>
 // https://material.io/resources/icons/?style=baseline
 import { mapMutations, mapState } from 'vuex'
+import SettingsButton from './SettingsButton.vue'
 
 export default {
   props: {
@@ -29,11 +51,16 @@ export default {
       required: true,
     },
   },
+  inject: ['theme'],
+  components: {
+    SettingsButton,
+  },
   computed: {
     ...mapState(['darkMode', 'densePadding']),
   },
   methods: {
     ...mapMutations(['setVuexState', 'saveAppSettings']),
+    // TODO https://vuetifyjs.com/en/features/theme/
     setDark() {
       const dark = !this.darkMode
       this.setVuexState({ k: 'darkMode', v: dark })
---
+++ src/components/app/SettingsButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-btn
+        v-on="on"
+        @click="() => $router.push(`/AppSettingsView`).catch((err) => {})"
+        icon
+      >
+        <v-icon>build</v-icon>
+      </v-btn>
+    </template>
+    <span>Search History</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {},
+  computed: {},
+  methods: {},
+}
+</script>
