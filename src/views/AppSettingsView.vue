b8a07beeff40f8f56ceca1e526375892536e70a7
Ed Pfromer <epfromer@gmail.com>
2020-12-04 09:46:36 -0700

    update

--- src/views/AppSettingsView.vue
+++ src/views/AppSettingsView.vue
@@ -10,14 +10,17 @@
         <custodian-settings />
       </v-col>
     </v-row>
+    <div class="headline">Import Log</div>
+    <import-log />
   </v-container>
 </template>
 
 <script>
 import CustodianSettings from '@/components/CustodianSettings.vue'
 import ThemePicker from '@/components/ThemePicker.vue'
+import ImportLog from '@/components/ImportLog.vue'
 
 export default {
-  components: { ThemePicker, CustodianSettings },
+  components: { ThemePicker, CustodianSettings, ImportLog },
 }
 </script>
