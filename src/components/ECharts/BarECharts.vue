ff881ec1dff05be1cd06379cdf355ba830c07718
Ed Pfromer <epfromer@gmail.com>
2021-03-06 07:55:25 -0700

    echarts

--- src/components/ECharts/BarECharts.vue
+++ src/components/ECharts/BarECharts.vue
@@ -1,14 +1,11 @@
 <template>
-  <!-- <v-chart v-if="config" :options="config" @click="onClick" autoresize /> -->
-  <figure>
-    <v-chart
-      :option="bar"
-      :init-options="initOptions"
-      autoresize
-      :loading="barLoading"
-      :loadingOptions="barLoadingOptions"
-    />
-  </figure>
+  <v-chart
+    v-if="config"
+    :option="config"
+    :init-options="initOptions"
+    autoresize
+    @click="onClick"
+  />
 </template>
 
 <script>
@@ -43,54 +40,7 @@ export default {
   data() {
     return {
       config: null,
-      bar: {
-        textStyle: {
-          fontFamily: 'Inter, "Helvetica Neue", Arial, sans-serif',
-        },
-        dataset: {
-          dimensions: ['Product', '2015', '2016', '2017'],
-          source: [
-            {
-              Product: 'Matcha Latte',
-              2015: this.random(),
-              2016: this.random(),
-              2017: this.random(),
-            },
-            {
-              Product: 'Milk Tea',
-              2015: this.random(),
-              2016: this.random(),
-              2017: this.random(),
-            },
-            {
-              Product: 'Cheese Cocoa',
-              2015: this.random(),
-              2016: this.random(),
-              2017: this.random(),
-            },
-            {
-              Product: 'Walnut Brownie',
-              2015: this.random(),
-              2016: this.random(),
-              2017: this.random(),
-            },
-          ],
-        },
-        xAxis: { type: 'category' },
-        yAxis: {},
-        // Declare several bar series, each will be mapped
-        // to a column of dataset.source by default.
-        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }],
-      },
-      initOptions: {
-        renderer: 'canvas',
-      },
-      barLoading: false,
-      barLoadingOptions: {
-        text: 'Loading…',
-        color: '#4ea397',
-        maskColor: 'rgba(255, 255, 255, 0.4)',
-      },
+      initOptions: { renderer: 'canvas' },
     }
   },
   props: {
@@ -112,25 +62,14 @@ export default {
     },
   },
   inject: ['theme'],
-  components: {
-    VChart,
-  },
+  components: { VChart },
   computed: {
     ...mapState(['darkMode']),
   },
   mounted() {
-    // this.createChart()
+    this.createChart()
   },
   methods: {
-    handleClickk(...args) {
-      console.log('click from echarts', ...args)
-    },
-    handleZrClick(...args) {
-      console.log('click from zrender', ...args)
-    },
-    random() {
-      return Math.round(300 + Math.random() * 700) / 10
-    },
     onClick(e) {
       if (e.data && e.data.name) this.handleClick(this.search, e.data.name)
     },
@@ -201,20 +140,3 @@ export default {
   },
 }
 </script>
-
-<style lang="postcss">
-figure {
-  width: 100vw;
-  margin: 1em auto;
-  padding: 1em 0;
-  border: none;
-  border-radius: 0;
-  box-shadow: none;
-
-  .echarts {
-    width: 100%;
-    min-width: 0;
-    height: 75vw;
-  }
-}
-</style>
